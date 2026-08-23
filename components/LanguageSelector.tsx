9801e4275116f97d5ce1699c4939ca71179b649f
Edward <arven.intell@gmail.com>
2026-08-23 23:52:39 +0300

    Update LanguageSelector.tsx

--- components/LanguageSelector.tsx
+++ components/LanguageSelector.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState } from 'react' 
 import { useRouter, usePathname } from 'next/navigation'
 import { ChevronDown } from 'lucide-react'
 
