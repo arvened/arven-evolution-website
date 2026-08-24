74453d40647e0d78a8b81892f487de7282a4eea0
Edward <arven.intell@gmail.com>
2026-08-24 23:12:00 +0300

    “Add next-intl config”

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const createNextIntlPlugin = require('next-intl/plugin');
 
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 module.exports = withNextIntl(nextConfig);
-
